0c3fb58cc3d138cfad4b22f18ce2c6ce6fabe87d
Jackie Maloney <jmaloney11277@gmail.com>
2024-10-09 10:00:46 -0400

    adds boiler plate footer content and adds to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import NavTabs from './components/NavTabs';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <main>
       <Outlet />
     </main>
+    <Footer />
     </>
   );
 }
