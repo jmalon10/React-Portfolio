2371f475def07e63c9ffdd86c8eb97447d3b6112
Jackie Maloney <jmaloney11277@gmail.com>
2024-10-29 15:28:42 -0400

    made profle picture an appropreate size and added styling to the contact form

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,14 @@ import profileImage from '../assets/ImageOfMe.jpg';
 export default function About() {
     return (
         <section>
-            <img src={profileImage} alt="Picture of me :)" />
+            <div className="text-center">
+                <img 
+                    src={profileImage} 
+                    className="rounded img-fluid" 
+                    alt="Picture of me :)" 
+                    style={{ maxWidth: '300px', height: 'auto' }} 
+                />
+            </div>
             <h2>About Me</h2>
             <p>Hi, I'm Jackie Maloney. I'm a full stack web developer with a passion for creating clean, user-friendly applications. I have experience with HTML, CSS, JavaScript, and React on the front end, and Node.js, Express.js, and MySQL on the back end. I'm excited to continue learning and growing as a developer!</p>
         </section>
